d1c0b784134817315d629059ed5481354c33df2f
stevenhuhpy <hufeixiahpy_selected@163.com>
2021-05-21 13:22:45 +0800

    import src of app

---
+++ src/SRTConvertor/CommonDef.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRTConvertor
+{
+    public class CommonDef
+    {
+        static public string Title = "SRT Convertor";
+    }
+}
